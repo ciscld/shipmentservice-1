b2983700c6ef2b70c6924238ca8a42624127cd28
PrinuNazeem <pnazee@gmail.com>
2020-08-12 22:50:32 -0700

    update

--- src/main/java/com/infosys/api/shipment/events/listeners/ItemCreatedEventListener.java
+++ src/main/java/com/infosys/api/shipment/events/listeners/ItemCreatedEventListener.java
@@ -1,26 +1,27 @@
 package com.infosys.api.shipment.events.listeners;
 
 import com.infosys.api.shipment.commands.SendShipment;
+import com.infosys.api.shipment.events.ItemCreatedEvent;
 import com.infosys.api.shipment.events.OrderCreatedEvent;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderCreatedEventListener {
+public class ItemCreatedEventListener {
     private final CommandGateway commandGateway;
 
-    public OrderCreatedEventListener(CommandGateway commandGateway) {
+    public ItemCreatedEventListener(CommandGateway commandGateway) {
         this.commandGateway = commandGateway;
     }
 
     @EventHandler
-    public void onEvent(OrderCreatedEvent event) {
+    public void onEvent(ItemCreatedEvent event) {
 
-        System.out.println("Received Order Created Event:" + event.getName() + " on thread named "
+        System.out.println("Received Order Created Event:" + event.getItemName() + " on thread named "
                 + Thread.currentThread().getName());
 
-        commandGateway.send(new SendShipment(event.getOrderIdentifier(), event.getName()));
+        commandGateway.send(new SendShipment(event.getCartIdentifier(), event.getItemName()));
         //return "Saved";
     }
 }
